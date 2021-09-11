498c7bcb0ff8dd7c9811f7c44b78b0356c5a758e
hieugaweb1 <nmh110499@gmail.com>
2021-09-11 14:56:31 +0700

    Ecommerce-Admin

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+export const config = {
+  api: {
+    url: 'http://localhost:3000/api/v1'
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyCAu9-RUNbDrxnKpNh2NVsb-lwY4yuiIhw",
+    authDomain: "test-sheets-project-app.firebaseapp.com",
+    databaseURL: "https://test-sheets-project-app-default-rtdb.firebaseio.com",
+    projectId: "test-sheets-project-app",
+    storageBucket: "test-sheets-project-app.appspot.com",
+    messagingSenderId: "537266275370",
+    appId: "1:537266275370:web:987f8b98e1d5d86b776f53",
+    measurementId: "G-PYFG97KCY6"
+  }
+}
